ad710becf3486f49ff6d829d27ebe5133d702400
Paul Kinlan <paul.kinlan@gmail.com>
2015-03-01 21:10:52 +0000

    Removing imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -40,10 +40,6 @@ gulp.task('jshint', function() {
 // Optimize Images
 gulp.task('images', function() {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}));
 });

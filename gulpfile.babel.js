141494b95ca6416464c1e86d0720ff6e420bd912
Paul Kinlan <paul.kinlan@gmail.com>
2017-06-10 23:04:56 +0100

    Start of the clear up of deps.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,14 +35,6 @@ import pkg from './package.json';
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
-// Lint JavaScript
-gulp.task('lint', () =>
-  gulp.src('app/scripts/**/*.js')
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failOnError()))
-);
-
 // Optimize images
 gulp.task('images', () =>
   gulp.src('app/images/**/*')
